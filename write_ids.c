91d38401e5fba209fb59a2abbe0d31ea7789cec5
bzt5255 <bzt5255@psu.edu>
2024-09-13 00:05:26 -0400

    Changes to write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "bzt5255";
+    char STUDENT_ID[] = "976638828";
 
     return 0 ;
 }
